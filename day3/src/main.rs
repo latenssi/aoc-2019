6320700121af2c835bd3bf7b0c6c2e6e5335a0d8
Lauri Junkkari <latenssi@gmail.com>
2020-04-24 14:29:22 +0300

    Update

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,3 +1,6 @@
+extern crate regex;
+
+use regex::Regex;
 use std::fs;
 
 #[derive(Debug)]
@@ -10,21 +13,34 @@ fn main() {
     let input_str = fs::read_to_string("input.txt").expect("Something went wrong reading the file");
     let paths: Vec<&str> = input_str.lines().collect();
 
-    println!("{:?}", path_to_points(paths[0]));
+    println!("{:?}", cvt_str2points(paths[0]));
 }
 
-fn path_to_points(path_str: &str) -> Vec<Point> {
+fn cvt_str2points(path_str: &str) -> Vec<Point> {
     let path: Vec<&str> = path_str.split(",").collect();
     let mut points: Vec<Point> = Vec::with_capacity(path.len() as usize);
+
+    let re = Regex::new(r"^(?P<direction>\w{1})(?P<steps>\d+)$").unwrap();
+
     for i in 0..path.len() {
-        match String::from(path[i]).chars().nth(0).as_ref() {
-            "R" => println!("Right"),
-            "L" => println!("Left"),
-            "U" => println!("Up"),
-            "D" => println!("Down"),
-            _ => println!("???"),
-        }
-        points.push(Point { x: 2, y: 1 })
+        assert!(re.is_match(path[i]));
+
+        let caps = re.captures(path[i]).unwrap();
+        let dir = caps.name("direction").map_or("", |m| m.as_str());
+        let steps = caps.name("steps").map_or("", |m| m.as_str());
+
+        println!("{:?}", (dir, steps));
+
+        // let matches: Vec<_> = path[i].matches(re).into_iter().collect();
+        // println!("{:?}", matches)
+        // match String::from(path[i]).chars().nth(0).as_ref() {
+        //    Some('R') => println!("Right {:?}", path[i]),
+        //    Some('L') => println!("Left"),
+        //    Some('U') => println!("Up"),
+        //    Some('D') => println!("Down"),
+        //     _ => println!("???"),
+        // }
+        // points.push(Point { x: 2, y: 1 })
     }
     points
 }
