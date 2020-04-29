89ef667bde93421309ed0d54b27a9adb570604d2
Lauri Junkkari <latenssi@gmail.com>
2020-04-29 13:59:52 +0300

    Day 4, part 2 wip

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -4,18 +4,18 @@ fn main() {
 
     println!(
         "Different passwords (part1): {}",
-        crack(input_start, input_end, -1)
+        crack(input_start, input_end, false)
     );
     println!(
         "Different passwords (part2): {}",
-        crack(input_start, input_end, 2)
+        crack(input_start, input_end, true)
     );
 }
 
-fn crack(start: u32, end: u32, max_grp_size: i32) -> u32 {
+fn crack(start: u32, end: u32, limit_grp_size: bool) -> u32 {
     (start..end + 1)
         .map(|pass| {
-            if validate(pass, start, end, max_grp_size) {
+            if validate(pass, start, end, limit_grp_size) {
                 1
             } else {
                 0
@@ -24,40 +24,42 @@ fn crack(start: u32, end: u32, max_grp_size: i32) -> u32 {
         .sum()
 }
 
-fn validate(pass: u32, start: u32, end: u32, max_grp_size: i32) -> bool {
+fn validate(pass: u32, start: u32, end: u32, limit_grp_size: bool) -> bool {
     let pass_str = pass.to_string();
 
     let v_length = pass_str.len() == 6;
     let v_range = start <= pass && pass <= end;
-    let mut v_incr = true;
+    let mut v_incr = false;
     let mut v_two_adj = false;
     let mut v_grp_size = false;
 
     let mut grp_size = 1;
 
     for (i, c) in pass_str.chars().enumerate() {
-        if i > 0 {
-            let l_d = (pass_str.chars().nth(i - 1).unwrap()).to_digit(10).unwrap();
-            let c_d = c.to_digit(10).unwrap();
-
-            v_incr = v_incr && l_d <= c_d;
-
-            if !v_incr {
-                break;
-            }
-
-            if l_d == c_d {
-                grp_size += 1;
-            } else {
-                grp_size = 1;
-            }
-
-            v_two_adj = v_two_adj || grp_size == 2;
-
-            v_grp_size = max_grp_size < 0
-                || (2 <= grp_size && grp_size <= max_grp_size)
-                || (v_grp_size && grp_size < 2);
+        if i == 0 {
+            continue;
         }
+
+        let l_d = (pass_str.chars().nth(i - 1).unwrap()).to_digit(10).unwrap();
+        let c_d = c.to_digit(10).unwrap();
+
+        v_incr = l_d <= c_d;
+
+        if !v_incr {
+            break;
+        }
+
+        if l_d == c_d {
+            grp_size += 1;
+        } else {
+            grp_size = 1;
+        }
+
+        let two_adj = grp_size == 2;
+
+        v_two_adj = v_two_adj || two_adj;
+
+        v_grp_size = !limit_grp_size || two_adj || (v_grp_size && grp_size < 2);
     }
 
     v_length && v_range && v_incr && v_two_adj && v_grp_size
@@ -65,30 +67,33 @@ fn validate(pass: u32, start: u32, end: u32, max_grp_size: i32) -> bool {
 
 #[test]
 fn test_validate1() {
-    assert_eq!(validate(111_111, 111_111, 111_111, -1), true);
-    assert_eq!(validate(223_450, 223_450, 223_450, -1), false);
-    assert_eq!(validate(123_789, 123_789, 123_789, -1), false);
+    assert_eq!(validate(111_111, 111_111, 111_111, false), true);
+    assert_eq!(validate(223_450, 223_450, 223_450, false), false);
+    assert_eq!(validate(123_789, 123_789, 123_789, false), false);
 }
 
 #[test]
 fn test_validate2() {
-    assert_eq!(validate(112_233, 112_233, 112_233, 2), true);
-    assert_eq!(validate(112_334, 112_334, 112_334, 2), true);
-    assert_eq!(validate(111_122, 111_122, 111_122, 2), true);
-    assert_eq!(validate(112_333, 112_333, 112_333, 2), false);
-    assert_eq!(validate(123_789, 123_789, 123_789, 2), false);
-    assert_eq!(validate(123_444, 123_444, 123_444, 2), false);
-    assert_eq!(validate(112_223, 112_223, 112_223, 2), false);
+    assert_eq!(validate(112_233, 112_233, 112_233, true), true);
+    assert_eq!(validate(112_334, 112_334, 112_334, true), true);
+    assert_eq!(validate(111_122, 111_122, 111_122, true), true);
+    assert_eq!(validate(111_223, 111_223, 111_223, true), true);
+    assert_eq!(validate(122_233, 122_233, 122_233, true), true);
+    assert_eq!(validate(122_234, 122_234, 122_234, true), false);
+    assert_eq!(validate(112_333, 112_333, 112_333, true), false);
+    assert_eq!(validate(123_789, 123_789, 123_789, true), false);
+    assert_eq!(validate(123_444, 123_444, 123_444, true), false);
+    assert_eq!(validate(112_223, 112_223, 112_223, true), false);
 }
 
 #[test]
 fn test_part1() {
-    assert_eq!(crack(111_111, 111_112, -1), 2);
-    assert_eq!(crack(353_096, 843_212, -1), 579);
+    assert_eq!(crack(111_111, 111_112, false), 2);
+    assert_eq!(crack(353_096, 843_212, false), 579);
 }
 
 #[test]
 fn test_part2() {
-    assert_eq!(crack(112_333, 112_334, 2), 1);
-    assert!(crack(353_096, 843_212, 2) > 274);
+    assert_eq!(crack(112_333, 112_334, true), 1);
+    assert!(crack(353_096, 843_212, true) > 274);
 }
