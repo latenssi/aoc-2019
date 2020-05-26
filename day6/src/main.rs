a99472f30eccffddd20b4db027137936e7f97fe5
Lauri Junkkari <latenssi@gmail.com>
2020-05-26 16:51:45 +0300

    It is not pretty but it works

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,121 +1,76 @@
-// use std::collections::LinkedList;
+use std::fs;
 
-// pub struct System {
-//     root: Orbit,
-// }
-
-// type Orbit = Option<Box<Orbiter>>;
+#[derive(Debug)]
+struct System {
+    orbiters: Vec<Orbiter>,
+}
 
 #[derive(Debug)]
 struct Orbiter {
-    key: char,
-    parent: Option<Box<Orbiter>>,
+    key: String,
+    orbits: String,
 }
 
-impl PartialEq for Orbiter {
-    fn eq(&self, other: &Self) -> bool {
-        self.key == other.key
+impl System {
+    fn add_orbiter(&mut self, key: String, orbits: String) {
+        self.orbiters.push(Orbiter { key, orbits });
+    }
+
+    fn parse_orbits(&mut self, input: &[String]) {
+        for line in input {
+            let mut s = line.split(')');
+
+            let p1 = s.next().unwrap();
+            let p2 = s.next().unwrap();
+
+            self.add_orbiter(p2.to_string(), p1.to_string());
+        }
+    }
+
+    fn path_len(&self, key: String) -> i32 {
+        let mut len = 0;
+        let mut next = key;
+        while let Some(orbiter) = self.orbiters.iter().find(|o| o.key == next) {
+            next = orbiter.orbits.to_owned();
+            len += 1;
+        }
+        len
+    }
+
+    fn total_orbits(&self) -> i32 {
+        self.orbiters
+            .iter()
+            .map(|o| self.path_len(o.key.to_string()))
+            .sum()
     }
 }
-
-impl Eq for Orbiter {}
-
-// impl Node {
-//     fn children<'a>(&self, nodes: &'a [Node]) -> Vec<&'a Node> {
-//         nodes
-//             .iter()
-//             .filter(|n| n.parent == self.elem)
-//             .collect::<Vec<&Node>>()
-//     }
-// }
-
-// impl TryFrom<&&str> for Orbiter {
-//     type Error = ();
-
-//     fn try_from(line: &&str) -> Result<Self, Self::Error> {
-//         Orbiter::try_from(line.split(')').collect::<Vec<&str>>())
-//     }
-// }
-
-// impl TryFrom<Vec<&str>> for Orbiter {
-//     type Error = ();
-
-//     fn try_from(split: Vec<&str>) -> Result<Self, Self::Error> {
-//         if split.len() == 2 {
-//             Ok(Orbiter {
-//                 key: split[1].parse().unwrap(),
-//                 parent: split[0].parse().unwrap(),
-//             })
-//         } else {
-//             Err(())
-//         }
-//     }
-// }
-
-// #[derive(Debug)]
-// struct Tree <'a>{
-//     root: String,
-//     nodes: &'a [Node]
-// }
-
-// impl Tree <'_> {
-//     fn generate(&self) {}
-// }
 
 fn main() {
-    let ex = [
-        "COM)B", "B)C", "C)D", "D)E", "E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L",
-    ];
+    let input_str = fs::read_to_string("input.txt").expect("Something went wrong reading the file");
 
-    parse_orbits(&ex);
+    let input: Vec<String> = input_str.lines().map(|l| (*l).to_string()).collect();
+
+    let mut system = System { orbiters: vec![] };
+
+    system.parse_orbits(&input);
+
+    println!("{:?}", system.total_orbits())
 }
-
-fn parse_orbits(input: &[&str]) {
-    let orbiters: Vec<Box<Orbiter>> = vec![];
-
-    for line in input {
-        let mut s = line.split(')');
-
-        let p = s.next().unwrap();
-        let o = s.next().unwrap();
-
-        let parent = orbiters.iter().find(|i| i.key == 'p');
-
-        // let parent = Orbiter {
-        //     key: 'p',
-        //     parent: None,
-        // };
-
-        let orbiter = Orbiter { key: 'o', parent };
-
-        println!("{:?}", (line, p, o));
-    }
-
-    // input
-    //     .iter()
-    //     .map(|orbit_str| {
-    //         println!("{:?}", orbit_str);
-    //     })
-    //     // .filter_map(Result::ok)
-    //     .collect();
-}
-
-// fn calculate_checksum(list: List) -> u32 {
-//     0
-// }
 
 #[test]
 fn part_1_example() {
-    let ex = [
+    let ex: Vec<String> = [
         "COM)B", "B)C", "C)D", "D)E", "E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L",
-    ];
+    ]
+    .iter()
+    .map(|l| (*l).to_string())
+    .collect();
 
-    parse_orbits(&ex);
+    let mut system = System { orbiters: vec![] };
 
-    // let l: LinkedList<char> = LinkedList::new();
+    system.parse_orbits(&ex);
 
-    // let list = List { root: "COM".to_string(), nodes: &parse_orbits(&ex) };
-
-    // println!("{:?}", list);
-    // assert_eq!(calculate_checksum(list), 42);
+    assert_eq!(system.path_len("D".to_string()), 3);
+    assert_eq!(system.path_len("L".to_string()), 7);
+    assert_eq!(system.total_orbits(), 42);
 }
