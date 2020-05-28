3ded084b0c7419451c44ea1bdee725498b50f7e5
Lauri Junkkari <latenssi@gmail.com>
2020-05-28 16:05:03 +0300

    Update

---
+++ day6/src/lib_binaryheap.rs
@@ -0,0 +1,174 @@
+use std::cmp::Ordering;
+use std::collections::{BinaryHeap, HashMap, HashSet};
+
+#[derive(Debug)]
+pub struct System {
+    nodes: HashSet<String>,
+    node_ids: HashMap<String, usize>,
+    adj: HashMap<usize, Vec<usize>>,
+}
+
+#[derive(Copy, Clone, Eq, PartialEq)]
+struct SearchState {
+    id: usize,
+    length: usize,
+}
+
+impl System {
+    pub fn new() -> Self {
+        System {
+            nodes: HashSet::new(),
+            node_ids: HashMap::new(),
+            adj: HashMap::new(),
+        }
+    }
+
+    pub fn parse_orbits(&mut self, input: &[&str]) {
+        for line in input {
+            let mut s = line.split(')');
+
+            let p1 = s.next().unwrap();
+            let p2 = s.next().unwrap();
+
+            self.add_orbiter(p2, p1);
+        }
+    }
+
+    pub fn total_orbits(&self) -> usize {
+        self.nodes
+            .iter()
+            .map(|o| self.depth(o))
+            .filter_map(Result::ok)
+            .sum()
+    }
+
+    pub fn depth(&self, o: &str) -> Result<usize, &'static str> {
+        match self.shortest_path(o, "COM") {
+            Some(d) => Ok(d),
+            None => Err("Unable to find path"),
+        }
+    }
+
+    pub fn minimun_orbital_transfers(
+        &self,
+        source: &str,
+        target: &str,
+    ) -> Result<usize, &'static str> {
+        // Calculating orbital transfers so decrease by 2 since the current
+        // orbits are taken into account when calculating paths
+        match self.shortest_path(source, target) {
+            Some(d) => Ok(d - 2),
+            None => Err("Unable to find path"),
+        }
+    }
+
+    fn shortest_path(&self, source: &str, target: &str) -> Option<usize> {
+        if !self.node_ids.contains_key(source) || !self.node_ids.contains_key(target) {
+            return None;
+        }
+
+        let s_id = *self.node_ids.get(source).unwrap();
+        let t_id = *self.node_ids.get(target).unwrap();
+
+        let mut dist: Vec<usize> = (0..self.node_ids.len()).map(|_| usize::MAX).collect();
+
+        let mut heap = BinaryHeap::new();
+
+        dist[s_id] = 0;
+
+        heap.push(SearchState {
+            id: s_id,
+            length: 0,
+        });
+
+        while let Some(SearchState { id, length }) = heap.pop() {
+            if id == t_id {
+                return Some(length);
+            }
+
+            if length > dist[id] {
+                continue;
+            }
+
+            for n in &self.adj[&id] {
+                let next = SearchState {
+                    length: length + 1,
+                    id: *n,
+                };
+
+                if next.length < dist[next.id] {
+                    heap.push(next);
+                    dist[next.id] = next.length;
+                }
+            }
+        }
+
+        None
+    }
+
+    fn add_orbiter(&mut self, o1: &str, o2: &str) {
+        if self.nodes.insert(o1.to_string()) {
+            // Assign an ID
+            self.node_ids.insert(o1.to_string(), self.node_ids.len());
+        }
+
+        if self.nodes.insert(o2.to_string()) {
+            // Assign an ID
+            self.node_ids.insert(o2.to_string(), self.node_ids.len());
+        }
+
+        self.adj
+            .entry(*self.node_ids.get(o1).unwrap())
+            .or_insert_with(Vec::new)
+            .push(*self.node_ids.get(o2).unwrap());
+
+        self.adj
+            .entry(*self.node_ids.get(o2).unwrap())
+            .or_insert_with(Vec::new)
+            .push(*self.node_ids.get(o1).unwrap());
+    }
+}
+
+impl Ord for SearchState {
+    fn cmp(&self, other: &SearchState) -> Ordering {
+        other
+            .length
+            .cmp(&self.length)
+            .then_with(|| self.id.cmp(&other.id))
+    }
+}
+
+impl PartialOrd for SearchState {
+    fn partial_cmp(&self, other: &SearchState) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+#[test]
+fn part_1_example() {
+    let ex = [
+        "COM)B", "B)C", "C)D", "D)E", "E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L",
+    ];
+
+    let mut system = System::new();
+
+    system.parse_orbits(&ex);
+
+    assert_eq!(system.depth("D").unwrap(), 3);
+    assert_eq!(system.depth("L").unwrap(), 7);
+    assert_eq!(system.total_orbits(), 42);
+}
+
+#[test]
+fn part_2_example() {
+    let ex = [
+        "COM)B", "B)C", "C)D", "D)E", "E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L", "K)YOU",
+        "I)SAN",
+    ];
+
+    let mut system = System::new();
+
+    system.parse_orbits(&ex);
+
+    assert_eq!(system.minimun_orbital_transfers("YOU", "SAN").unwrap(), 4);
+}
--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,6 +1,7 @@
 use std::fs;
 use std::time::Instant;
 
+pub mod lib_binaryheap;
 pub mod lib_bruteforce;
 pub mod lib_hashmap;
 
@@ -9,23 +10,64 @@ fn main() {
 
     let input: Vec<&str> = input_str.lines().collect();
 
+    // Way 1
+
+    // let t0 = Instant::now();
+    // let mut system1 = lib_bruteforce::System::new();
+    // system1.parse_orbits(&input);
+    // println!("Way 1");
+    // println!("Answer part 1: {}", &system1.total_orbits());
+    // println!("Duration: {}ms", t0.elapsed().as_millis());
+
+    // println!();
+
+    // Way 2
+
+    let mut system = lib_hashmap::System::new();
+    println!("Way 2");
+
+    let t1 = Instant::now();
+
     let t0 = Instant::now();
-    let mut system1 = lib_bruteforce::System::new();
-    system1.parse_orbits(&input);
-    println!("Old way");
-    println!("Answer part 1: {}", &system1.total_orbits());
+    system.parse_orbits(&input);
     println!("Duration: {}ms", t0.elapsed().as_millis());
+
+    let t0 = Instant::now();
+    println!("Answer part 1: {}", &system.total_orbits());
+    println!("Duration: {}ms", t0.elapsed().as_millis());
+
+    let t0 = Instant::now();
+    println!(
+        "Answer part 2: {:?}",
+        &system.minimun_orbital_transfers("YOU", "SAN").unwrap()
+    );
+    println!("Duration: {}ms", t0.elapsed().as_millis());
+
+    println!("Total duration: {}ms", t1.elapsed().as_millis());
 
     println!();
 
+    // Way 3
+
+    let mut system3 = lib_binaryheap::System::new();
+    println!("Way 3");
+
+    let t1 = Instant::now();
+
     let t0 = Instant::now();
-    let mut system2 = lib_hashmap::System::new();
-    system2.parse_orbits(&input);
-    println!("New way");
-    println!("Answer part 1: {}", &system2.total_orbits());
+    system3.parse_orbits(&input);
+    println!("Duration: {}ms", t0.elapsed().as_millis());
+
+    let t0 = Instant::now();
+    println!("Answer part 1: {}", &system3.total_orbits());
+    println!("Duration: {}ms", t0.elapsed().as_millis());
+
+    let t0 = Instant::now();
     println!(
         "Answer part 2: {:?}",
-        &system2.minimun_orbital_transfers("YOU", "SAN").unwrap()
+        &system3.minimun_orbital_transfers("YOU", "SAN").unwrap()
     );
     println!("Duration: {}ms", t0.elapsed().as_millis());
+
+    println!("Total duration: {}ms", t1.elapsed().as_millis());
 }
